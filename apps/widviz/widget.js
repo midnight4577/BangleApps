060029a0fb0f9d28c591c2f7090b7dd59ed460fc
midnight4577 <45957548+midnight4577@users.noreply.github.com>
2021-04-12 09:49:50 -0700

    Tried to remove the icon
    probaly failed

--- apps/widviz/widget.js
+++ apps/widviz/widget.js
@@ -9,8 +9,6 @@
       saved.push(wd.draw); 
       wd.draw=()=>{};
     }
-    g.setColor(0,0,0);
-    g.fillRect(0,0,239,23);
   }
   
   function reveal(){
@@ -19,15 +17,7 @@
     Bangle.drawWidgets(); 
     saved=null;
   }
-  
-  function draw(){
-    var img = E.toArrayBuffer(atob("GBgBAAAAAAAAAAAAAAAAAH4AAf+AB4HgDgBwHDw4OH4cMOcMYMMGYMMGMOcMOH4cHDw4DgBwB4HgAf+AAH4AAAAAAAAAAAAAAAAA"));
-    g.setColor(0x07ff);
-    g.drawImage(img,this.x,this.y);
-  }
     
-  WIDGETS["viz"] ={area:"tl", width:24,draw:draw};
-
   Bangle.on('swipe',(dir)=>{
     if (dir<0) hide(); else reveal();
   });    
