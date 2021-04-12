f18ce6b40c34ca6ecee6f0f2d3fb46f3d8747d90
midnight4577 <45957548+midnight4577@users.noreply.github.com>
2021-04-12 09:56:01 -0700

    Update widget.js

--- apps/widviz/widget.js
+++ apps/widviz/widget.js
@@ -9,6 +9,8 @@
       saved.push(wd.draw); 
       wd.draw=()=>{};
     }
+    g.setColor(0,0,0);
+    g.fillRect(0,0,239,23);
   }
   
   function reveal(){
@@ -17,7 +19,14 @@
     Bangle.drawWidgets(); 
     saved=null;
   }
+  
+  function draw(){
+    var img = E.toArrayBuffer(atob("GBgBAAAAAAAAAAAAAAAAAH4AAf+AB4HgDgBwHDw4OH4cMOcMYMMGYMMGMOcMOH4cHDw4DgBwB4HgAf+AAH4AAAAAAAAAAAAAAAAA"));
+    g.setColor(0x07ff);
+  }
     
+  WIDGETS["viz"] ={area:"tl", width:24,draw:draw};
+
   Bangle.on('swipe',(dir)=>{
     if (dir<0) hide(); else reveal();
   });    
